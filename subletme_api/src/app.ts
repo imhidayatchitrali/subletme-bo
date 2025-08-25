ef575215b3b0038030959e9e56342306bb1bb0e8
imhidayatchitrali <hidayat.rehman64@gmail.com>
2025-08-25 16:06:20 +0500

    increase limit

--- subletme_api/src/app.ts
+++ subletme_api/src/app.ts
@@ -15,8 +15,8 @@ export const upload = multer({
 });
 
 app.use(cors());
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.get('/health', (req: Request, res: Response) => {
     res.json({ status: 'ok' });
 });
